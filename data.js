0dd57c09c266602a3ee385bd4248a333efe9c4b8
sc0701 <t01046341946@gmail.com>
2025-12-11 22:45:16 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    // 1. 기본 정보
+    names: "YUJIN <span class='heart'>❤</span> SEUNGCHAN", // 이름
+    startDate: "2025-12-06", // 사귄 날짜 (YYYY-MM-DD)
+    
+    // 2. 앨범 목록
+    albums: [
+        { name: "2025.12", folder: "2025.12"},
+        // { name: "2026.01", folder: "2026.01" },
+    ],
+
+    // 3. [NEW] 타임라인/히스토리
+    timeline: [
+        { date: "2025.12.06", title: "우리의 시작", desc: "우리가 처음 연인이 된 날 ✨" },   
+    ]
+};
